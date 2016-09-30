7b81e9f25d776d147e16cbaec975a5dec32b75d9
luca.montanari <luca.montanari@manageddesigns.it>
2016-09-30 08:36:43 +0200

    Changed Flash icon

---
+++ FreeTools/Converters/FixedAlphaBrushColorConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace FreeTools.Converters
+{
+    public class FixedAlphaBrushColorConverter : DependecyObject, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Color)
+            {
+                var color = (Color)value;
+                return Color.FromArgb(
+                    System.Convert.ToByte(color.A * 0.75), 
+                    color.R, 
+                    color.G, 
+                    color.B);
+            }
+            else if (value is SolidColorBrush)
+            {
+                var solidColorBrush = (SolidColorBrush)value;
+                return new SolidColorBrush(
+                    Color.FromArgb(
+                        System.Convert.ToByte(solidColorBrush.Color.A * 0.75), 
+                        solidColorBrush.Color.R, 
+                        solidColorBrush.Color.G, 
+                        solidColorBrush.Color.B));
+            }
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
